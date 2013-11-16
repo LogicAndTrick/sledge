33ffd730d354de2dfc9b5aba4dccfb805cdd911b
Daniel Walder <daniel.walder@gmail.com>
2013-11-16 14:30:48 +1000

    Render the camera tool in the 2D views

--- Sledge.Editor/Tools/CameraTool.cs
+++ Sledge.Editor/Tools/CameraTool.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using Sledge.DataStructures.Geometric;
 using Sledge.Editor.Properties;
+using Sledge.Editor.UI;
+using Sledge.Graphics;
 using Sledge.Settings;
 using Sledge.UI;
+using Tao.OpenGl;
 
 namespace Sledge.Editor.Tools
 {
@@ -78,7 +84,69 @@ namespace Sledge.Editor.Tools
 
         public override void Render(ViewportBase viewport)
         {
-            //
+            var vp = viewport as Viewport2D;
+            if (vp == null) return;
+
+            var cam = ViewportManager.Viewports.OfType<Viewport3D>().Select(x => x.Camera).FirstOrDefault();
+            if (cam == null) return;
+
+            var z = (double)vp.Zoom;
+            var pos = new Coordinate((decimal)cam.Location.X, (decimal)cam.Location.Y, (decimal)cam.Location.Z);
+            var look = new Coordinate((decimal)cam.LookAt.X, (decimal)cam.LookAt.Y, (decimal)cam.LookAt.Z);
+            var p1 = vp.Flatten(pos);
+            var p2 = p1 + (vp.Flatten(look) - p1).Normalise() * 50 / vp.Zoom;
+
+            var multiplier = 4 / vp.Zoom;
+            var dir = (p2 - p1).Normalise();
+            var cp = new Coordinate(-dir.Y, dir.X, 0).Normalise();
+            
+            GL.Enable(EnableCap.LineSmooth);
+            GL.Hint(HintTarget.LineSmoothHint, HintMode.Nicest);
+
+            // Draw lines between points and point outlines
+            GL.Begin(BeginMode.Lines);
+
+            GL.Color3(Color.Red);
+            GL.Vertex2(p1.DX, p1.DY);
+            GL.Vertex2(p2.DX, p2.DY);
+            GL.Vertex2(p2.DX, p2.DY);
+            GL.Vertex2(p1.DX, p1.DY);
+
+            GL.Color3(Color.Black);
+            GLX.Circle(new Vector2d(p1.DX, p1.DY), 4, z);
+            Coord(p2 + dir * 1.5m * multiplier);
+            Coord(p2 - (dir + cp) * multiplier);
+            Coord(p2 - (dir + cp) * multiplier);
+            Coord(p2 - (dir - cp) * multiplier);
+            Coord(p2 - (dir - cp) * multiplier);
+            Coord(p2 + dir * 1.5m * multiplier);
+
+            GL.End();
+
+
+            GL.Enable(EnableCap.PolygonSmooth);
+            GL.Hint(HintTarget.PolygonSmoothHint, HintMode.Nicest);
+
+            // Position circle
+            GL.Begin(BeginMode.Polygon);
+            GL.Color3(Color.Cyan);
+            GLX.Circle(new Vector2d(p1.DX, p1.DY), 4, z, loop: true);
+            GL.End();
+
+            // Direction Triangle
+            GL.Begin(BeginMode.Triangles);
+            GL.Color3(Color.LawnGreen);
+            Coord(p2 + dir * 1.5m * multiplier);
+            Coord(p2 - (dir + cp) * multiplier);
+            Coord(p2 - (dir - cp) * multiplier);
+            GL.End();
+
+            GL.Disable(EnableCap.PolygonSmooth);
+        }
+
+        protected static void Coord(Coordinate c)
+        {
+            GL.Vertex3(c.DX, c.DY, c.DZ);
         }
 
         public override HotkeyInterceptResult InterceptHotkey(HotkeysMediator hotkeyMessage)
