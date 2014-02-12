98f7e62b5eb514004c899fde17a88fd4825a8974
Mozartkugeln <konstantinub@gmail.com>
2014-02-12 20:14:17 +0100

    Added curved ramp functionality for arches
    Example: https://dl.dropboxusercontent.com/u/17850283/challenge.png (made by hand)

--- Sledge.Editor/Brushes/ArchBrush.cs
+++ Sledge.Editor/Brushes/ArchBrush.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using Sledge.DataStructures.Geometric;
@@ -13,9 +13,11 @@ namespace Sledge.Editor.Brushes
     {
         private readonly NumericControl _arc;
         private readonly NumericControl _startAngle;
+        private readonly NumericControl _tiltAngle;
         private readonly NumericControl _wallWidth;
         private readonly NumericControl _numSides;
         private readonly NumericControl _addHeight;
+        private readonly BooleanControl _curvedRamp;
 
         public ArchBrush()
         {
@@ -23,7 +25,9 @@ namespace Sledge.Editor.Brushes
             _wallWidth = new NumericControl(this) { LabelText = "Wall width", Minimum = 1, Maximum = 1024, Value = 16};
             _numSides = new NumericControl(this) { LabelText = "Num. sides" };
             _startAngle = new NumericControl(this) { LabelText = "Start Angle", Minimum = 0, Maximum = 359, Value = 0 };
+            _tiltAngle = new NumericControl(this) { LabelText = "Tilt angle", Minimum = -90, Maximum = 90, Value = 0 };
             _addHeight = new NumericControl(this) { LabelText = "Add height", Minimum = -1024, Maximum = 1024, Value = 0};
+            _curvedRamp = new BooleanControl(this) { LabelText = "Curved ramp", Checked = false };
         }
 
         public string Name
@@ -35,9 +39,11 @@ namespace Sledge.Editor.Brushes
         {
             yield return _numSides;
             yield return _startAngle;
+            yield return _tiltAngle;
             yield return _arc;
             yield return _wallWidth;
             yield return _addHeight;
+            yield return _curvedRamp;
         }
 
         private Solid MakeSolid(IDGenerator generator, IEnumerable<Coordinate[]> faces, ITexture texture, Color col)
@@ -67,55 +73,88 @@ namespace Sledge.Editor.Brushes
             if (numsides < 3) yield break;
             var startAngle = (int)_startAngle.GetValue();
             if (startAngle < 0 || startAngle > 359) yield break;
+            var tiltAngle = (int)_tiltAngle.GetValue();
+            if (tiltAngle < -90 || startAngle > 90) yield break;
             var arc = (int)_arc.GetValue();
             if (arc < 1) yield break;
             var wallWidth = _wallWidth.GetValue();
             if (wallWidth < 1) yield break;
-            var archHeight = _addHeight.GetValue();
+            var addHeight = _addHeight.GetValue() / numsides;
+            var curvedRamp = _curvedRamp.GetValue();
 
-            // Very similar to the pipe brush, except with options for start angle, arc, and height
+            // Very similar to the pipe brush, except with options for start angle, tilt, arc, and height
             var width = box.Width;
             var length = box.Length;
             var height = box.Height;
+
             var majorOut = width / 2;
             var majorIn = majorOut - wallWidth;
             var minorOut = length / 2;
             var minorIn = minorOut - wallWidth;
 
             var start = DMath.DegreesToRadians(startAngle);
+            var tilt = DMath.DegreesToRadians(tiltAngle * Math.Sign(addHeight)); // Sign depends on curve direction
             var angle = DMath.DegreesToRadians(arc) / numsides;
-            var addHeight = archHeight / numsides;
 
             var colour = Colour.GetRandomBrushColour();
 
-            // Calculate the X and Y points for the inner and outer ellipses
+            // Calculate the coordinates of the inner and outer ellipses' points
             var outer = new Coordinate[numsides + 1];
             var inner = new Coordinate[numsides + 1];
             for (var i = 0; i < numsides + 1; i++)
             {
                 var a = start + i * angle;
+                var h = i * addHeight
+                var hypot = 0.5 * wallWidth
+
                 var xval = box.Center.X + majorOut * DMath.Cos(a);
                 var yval = box.Center.Y + minorOut * DMath.Sin(a);
-                var zval = box.Start.Z;
+                var zval = box.Start.Z + h + hypot * DMath.Sin(tiltAngle); // TODO: Interpolation
                 outer[i] = new Coordinate(xval, yval, zval).Round(0);
+
                 xval = box.Center.X + majorIn * DMath.Cos(a);
                 yval = box.Center.Y + minorIn * DMath.Sin(a);
+                zval = box.Start.Z + h - hypot * DMath.Sin(tiltAngle);     // TODO: Interpolation
                 inner[i] = new Coordinate(xval, yval, zval).Round(0);
             }
 
             // Create the solids
-            var z = new Coordinate(0, 0, height);
-            for (var i = 0; i < numsides; i++)
+            for (var i = 0; i < 2 * numsides; i++)
             {
-                var vertical = Coordinate.UnitZ * addHeight * i;
                 var faces = new List<Coordinate[]>();
-                var next = i + 1;
-                faces.Add(new[] { outer[i], outer[i] + z, outer[next] + z, outer[next] }.Select(x => x + vertical).ToArray());
-                faces.Add(new[] { inner[next], inner[next] + z, inner[i] + z, inner[i] }.Select(x => x + vertical).ToArray());
-                faces.Add(new[] { outer[next], outer[next] + z, inner[next] + z, inner[next] }.Select(x => x + vertical).ToArray());
-                faces.Add(new[] { inner[i], inner[i] + z, outer[i] + z, outer[i] }.Select(x => x + vertical).ToArray());
-                faces.Add(new[] { inner[next] + z, outer[next] + z, outer[i] + z, inner[i] + z }.Select(x => x + vertical).ToArray());
-                faces.Add(new[] { inner[i], outer[i], outer[next], inner[next] }.Select(x => x + vertical).ToArray());
+                var z = new Coordinate(0, 0, height);
+
+                if (curvedRamp)
+                {
+                    // Since we are triangulating/splitting each arch segment, we need to generate 2 brushes per side
+                    // TODO: Different split orientations for the segments, depending on the arch's curving direction
+                    if (i % 2 == 0)
+                    {
+                        faces.Add(new[] { inner[i],       outer[i],       inner[i] + z   });
+                        faces.Add(new[] { inner[i],       inner[i+1],     inner[i] + z   });
+                        faces.Add(new[] { outer[i],       inner[i+1],     outer[i] + z   });
+                        faces.Add(new[] { inner[i],       outer[i],       inner[i+1]     });
+                        faces.Add(new[] { inner[i] + z,   outer[i] + z,   inner[i+1] + z });
+                    }
+                    else
+                    {
+                        faces.Add(new[] { outer[i],       inner[i+1],     outer[i] + z   });
+                        faces.Add(new[] { outer[i],       outer[i+1],     outer[i] + z   });
+                        faces.Add(new[] { inner[i+1],     outer[i+1],     inner[i+1] + z });
+                        faces.Add(new[] { outer[i],       inner[i+1],     outer[i+1]     });
+                        faces.Add(new[] { outer[i] + z,   inner[i+1] + z, outer[i+1] + z });
+                    }
+                }
+                else
+                {
+                    faces.Add(new[] { outer[i],       outer[i] + z,   outer[i+1] + z, outer[i+1]   });
+                    faces.Add(new[] { inner[i+1],     inner[i+1] + z, inner[i] + z,   inner[i]     });
+                    faces.Add(new[] { outer[i+1],     outer[i+1] + z, inner[i+1] + z, inner[i+1]   });
+                    faces.Add(new[] { inner[i],       inner[i] + z,   outer[i] + z,   outer[i]     });
+                    faces.Add(new[] { inner[i+1] + z, outer[i+1] + z, outer[i] + z,   inner[i] + z });
+                    faces.Add(new[] { inner[i],       outer[i],       outer[i+1],     inner[i+1]   });
+                }
+
                 yield return MakeSolid(generator, faces, texture, colour);
             }
         }
