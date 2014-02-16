8ab43c99ce935a9717760445dce67b627c03c77f
Mozartkugeln <konstantinub@gmail.com>
2014-02-16 18:17:33 +0100

    Changed & fixed how arch brush solids are created

--- Sledge.Editor/Brushes/ArchBrush.cs
+++ Sledge.Editor/Brushes/ArchBrush.cs
@@ -69,22 +69,22 @@ namespace Sledge.Editor.Brushes
             solid.UpdateBoundingBox();
             return solid;
         }
-
+        
         public IEnumerable<MapObject> Create(IDGenerator generator, Box box, ITexture texture)
         {
-            var numsides = (int)_numSides.GetValue();
-            if (numsides < 3) yield break;
+            var numSides = (int)_numSides.GetValue();
+            if (numSides < 3) yield break;
             var wallWidth = _wallWidth.GetValue();
             if (wallWidth < 1) yield break;
             var arc = (int)_arc.GetValue();
             if (arc < 1) yield break;
             var startAngle = (int)_startAngle.GetValue();
             if (startAngle < 0 || startAngle > 359) yield break;
-            var addHeight = _addHeight.GetValue() / numsides;
+            var addHeight = _addHeight.GetValue();
             var curvedRamp = _curvedRamp.GetValue();
-            var tiltAngle = (int)_tiltAngle.GetValue();
-            if (tiltAngle < -90 || startAngle > 90) yield break;
-
+            var tiltAngle = curvedRamp ? (int)_tiltAngle.GetValue() : 0;
+            if (tiltAngle < -90 || tiltAngle > 90) yield break;
+            
             // Very similar to the pipe brush, except with options for start angle, tilt, arc, and height
             var width = box.Width;
             var length = box.Length;
@@ -96,33 +96,34 @@ namespace Sledge.Editor.Brushes
             var minorIn = minorOut - wallWidth;
 
             var start = DMath.DegreesToRadians(startAngle);
-            var tilt = DMath.DegreesToRadians(tiltAngle * Math.Sign(addHeight)); // Sign depends on curve direction
-            var angle = DMath.DegreesToRadians(arc) / numsides;
+            var tilt = DMath.DegreesToRadians(tiltAngle);
+            var angle = DMath.DegreesToRadians(arc) / numSides;
 
             var colour = Colour.GetRandomBrushColour();
 
             // Calculate the coordinates of the inner and outer ellipses' points
-            var outer = new Coordinate[numsides + 1];
-            var inner = new Coordinate[numsides + 1];
-            for (var i = 0; i < numsides + 1; i++)
+            // TODO: Handle height adding for both curving and non-curving arches in a separate method
+            var outer = new Coordinate[numSides + 1];
+            var inner = new Coordinate[numSides + 1];
+            for (var i = 0; i < numSides + 1; i++)
             {
                 var a = start + i * angle;
                 var h = i * addHeight;
-                var hypot = wallWidth / 2;
-
+                var tiltHeight = wallWidth / 2 * DMath.Sin(tilt); // TODO: Interpolation
+                
                 var xval = box.Center.X + majorOut * DMath.Cos(a);
                 var yval = box.Center.Y + minorOut * DMath.Sin(a);
-                var zval = box.Start.Z + h + hypot * DMath.Sin(tilt); // TODO: Interpolation
+                var zval = box.Start.Z + (curvedRamp ? h + tiltHeight : 0);
                 outer[i] = new Coordinate(xval, yval, zval).Round(0);
 
                 xval = box.Center.X + majorIn * DMath.Cos(a);
                 yval = box.Center.Y + minorIn * DMath.Sin(a);
-                zval = box.Start.Z + h - hypot * DMath.Sin(tilt);     // TODO: Interpolation
+                zval = box.Start.Z + (curvedRamp ? h - tiltHeight : 0);
                 inner[i] = new Coordinate(xval, yval, zval).Round(0);
             }
 
             // Create the solids
-            for (var i = 0; i < numsides; i++)
+            for (var i = 0; i < numSides; i++)
             {
                 var faces = new List<Coordinate[]>();
                 var z = new Coordinate(0, 0, height);
@@ -130,28 +131,32 @@ namespace Sledge.Editor.Brushes
                 if (curvedRamp)
                 {
                     // Since we are triangulating/splitting each arch segment, we need to generate 2 brushes per side
-                    faces.Add(new[] { inner[i],       inner[i] + z,   outer[i] + z,   outer[i]   });
+                    // TODO: Different split orientations for the segments depending on the arch's curving direction
                     faces.Add(new[] { inner[i+1],     inner[i+1] + z, inner[i] + z,   inner[i]   });
                     faces.Add(new[] { outer[i],       outer[i] + z,   inner[i+1] + z, inner[i+1] });
-                    faces.Add(new[] { inner[i],       outer[i],       inner[i+1]      });
+                    faces.Add(new[] { inner[i],       inner[i] + z,   outer[i] + z,   outer[i]   });
                     faces.Add(new[] { inner[i] + z,   inner[i+1] + z, outer[i] + z    });
+                    faces.Add(new[] { inner[i],       outer[i],       inner[i+1]      });
                     yield return MakeSolid(generator, faces, texture, colour);
 
-                    faces.Add(new[] { inner[i+1],     inner[i+1] + z, outer[i] + z,   outer[i]   });
+                    faces.Clear();
+
                     faces.Add(new[] { outer[i],       outer[i] + z,   outer[i+1] + z, outer[i+1] });
+                    faces.Add(new[] { inner[i+1],     inner[i+1] + z, outer[i] + z,   outer[i]   });
                     faces.Add(new[] { outer[i+1],     outer[i+1] + z, inner[i+1] + z, inner[i+1] });
-                    faces.Add(new[] { outer[i],       outer[i+1],     inner[i+1]      });
                     faces.Add(new[] { outer[i] + z,   inner[i+1] + z, outer[i+1] + z  });
+                    faces.Add(new[] { outer[i],       outer[i+1],     inner[i+1]      });
                     yield return MakeSolid(generator, faces, texture, colour);
                 }
                 else
                 {
-                    faces.Add(new[] { outer[i],       outer[i] + z,   outer[i+1] + z, outer[i+1]   });
-                    faces.Add(new[] { inner[i+1],     inner[i+1] + z, inner[i] + z,   inner[i]     });
-                    faces.Add(new[] { outer[i+1],     outer[i+1] + z, inner[i+1] + z, inner[i+1]   });
-                    faces.Add(new[] { inner[i],       inner[i] + z,   outer[i] + z,   outer[i]     });
-                    faces.Add(new[] { inner[i+1] + z, outer[i+1] + z, outer[i] + z,   inner[i] + z });
-                    faces.Add(new[] { inner[i],       outer[i],       outer[i+1],     inner[i+1]   });
+                    var h = i * addHeight * Coordinate.UnitZ;
+                    faces.Add(new[] { outer[i],       outer[i] + z,   outer[i+1] + z, outer[i+1]   }.Select(x => x + h).ToArray());
+                    faces.Add(new[] { inner[i+1],     inner[i+1] + z, inner[i] + z,   inner[i]     }.Select(x => x + h).ToArray());
+                    faces.Add(new[] { outer[i+1],     outer[i+1] + z, inner[i+1] + z, inner[i+1]   }.Select(x => x + h).ToArray());
+                    faces.Add(new[] { inner[i],       inner[i] + z,   outer[i] + z,   outer[i]     }.Select(x => x + h).ToArray());
+                    faces.Add(new[] { inner[i+1] + z, outer[i+1] + z, outer[i] + z,   inner[i] + z }.Select(x => x + h).ToArray());
+                    faces.Add(new[] { inner[i],       outer[i],       outer[i+1],     inner[i+1]   }.Select(x => x + h).ToArray());
                     yield return MakeSolid(generator, faces, texture, colour);
                 }
             }
