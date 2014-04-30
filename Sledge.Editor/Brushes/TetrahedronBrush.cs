012cac03ec8b2abc5fd7d84d0c1957d9fa83c9af
Mozartkugeln <konstantinub@gmail.com>
2014-04-30 20:45:48 +0200

    Tetrahedron's top vert can be placed at centroid
    Also changed the tetrahedron brush's vertices so that it points up
    rather than down.

--- Sledge.Editor/Brushes/TetrahedronBrush.cs
+++ Sledge.Editor/Brushes/TetrahedronBrush.cs
@@ -9,6 +9,13 @@ namespace Sledge.Editor.Brushes
 {
     public class TetrahedronBrush : IBrush
     {
+        private readonly BooleanControl _useCentroid;
+
+        public TetrahedronBrush()
+        {
+            _useCentroid = new BooleanControl(this) { LabelText = "Top vertex at centroid", Checked = false };
+        }
+
         public string Name
         {
             get { return "Tetrahedron"; }
@@ -16,33 +23,37 @@ namespace Sledge.Editor.Brushes
 
         public IEnumerable<BrushControl> GetControls()
         {
-            return new List<BrushControl>();
+            yield return _useCentroid;
         }
 
         public IEnumerable<MapObject> Create(IDGenerator generator, Box box, ITexture texture, int roundDecimals)
         {
+            var useCentroid = _useCentroid.GetValue();
+
+            // The lower Z plane will be the triangle, with the lower Y value getting the two corners
+            var c1 = new Coordinate(box.Start.X, box.Start.Y, box.Start.Z).Round(roundDecimals);
+            var c2 = new Coordinate(box.End.X, box.Start.Y, box.Start.Z).Round(roundDecimals);
+            var c3 = new Coordinate(box.Center.X, box.End.Y, box.Start.Z).Round(roundDecimals);
+            var centroid = new Coordinate((c1.X + c2.X + c3.X) / 3, (c1.Y + c2.Y + c3.Y) / 3, box.End.Z);
+            var c4 = (useCentroid ? centroid : new Coordinate(box.Center.X, box.Center.Y, box.End.Z)).Round(roundDecimals);
+
+            var faces = new[] {
+                new[] { c1, c2, c3 },
+                new[] { c4, c1, c3 },
+                new[] { c4, c3, c2 },
+                new[] { c4, c2, c1 }
+            };
+
             var solid = new Solid(generator.GetNextObjectID()) { Colour = Colour.GetRandomBrushColour() };
-            // The higher Z plane will be triangle, with the lower X value getting the two corners
-            var c1 = new Coordinate(box.Start.X, box.Start.Y, box.End.Z).Round(roundDecimals);
-            var c2 = new Coordinate(box.End.X, box.Start.Y, box.End.Z).Round(roundDecimals);
-            var c3 = new Coordinate(box.Center.X, box.End.Y, box.End.Z).Round(roundDecimals);
-            var c4 = new Coordinate(box.Center.X, box.Center.Y, box.Start.Z).Round(roundDecimals);
-            var faces = new[]
-                            {
-                                new[] { c3, c2, c1 },
-                                new[] { c3, c1, c4 },
-                                new[] { c2, c3, c4 },
-                                new[] { c1, c2, c4 }
-                            };
             foreach (var arr in faces)
             {
                 var face = new Face(generator.GetNextFaceID())
-                               {
-                                   Parent = solid,
-                                   Plane = new Plane(arr[0], arr[1], arr[2]),
-                                   Colour = solid.Colour,
-                                   Texture = { Texture = texture }
-                               };
+                {
+                    Parent = solid,
+                    Plane = new Plane(arr[0], arr[1], arr[2]),
+                    Colour = solid.Colour,
+                    Texture = { Texture = texture }
+                };
                 face.Vertices.AddRange(arr.Select(x => new Vertex(x, face)));
                 face.UpdateBoundingBox();
                 face.AlignTextureToFace();
