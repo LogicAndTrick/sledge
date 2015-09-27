95ce79c61b153dffa35b8b01fcebd8ef6f6f3b64
Eric Wasylishen <ewasylishen@gmail.com>
2015-09-27 16:20:49 -0600

    Wad: support loading WAD2 (Quake) texture wads.
    WAD2 is the same as WAD3 except that the miptex entries don't have a
    palette after the pixel data, the palette needs to be supplied externally.
    
    My implementation is lame / hacky, in that WadImageStream just looks for
    the quake palette in {exe}/Palettes/quake.lmp. I included the palette with
    this commit, it's supposed to be public domain
    (see http://quakewiki.org/wiki/Quake_palette#Legal_status)
    
    It would be cleaner to read from the user's quake directory, and this
    would also allow displaying textures for a Quake TC that uses its
    own palette.

--- Sledge.Packages/Wad/WadPackage.cs
+++ Sledge.Packages/Wad/WadPackage.cs
@@ -11,7 +11,8 @@ namespace Sledge.Packages.Wad
     // https://developer.valvesoftware.com/wiki/WAD
     public class WadPackage : IPackage
     {
-        private const string Signature = "WAD3";
+        private const string WAD2Signature = "WAD2";
+        private const string WAD3Signature = "WAD3";
 
         public FileInfo PackageFile { get; private set; }
         internal uint NumTextures { get; private set; }
@@ -27,7 +28,7 @@ namespace Sledge.Packages.Wad
             using (var br = new BinaryReader(OpenFile(packageFile)))
             {
                 var sig = br.ReadFixedLengthString(Encoding.ASCII, 4);
-                if (sig != Signature) throw new PackageException("Unknown package signature: Expected '" + Signature + "', got '" + sig + "'.");
+                if (sig != WAD2Signature && sig != WAD3Signature) throw new PackageException("Unknown package signature: Expected [WAD2,WAD3], got '" + sig + "'.");
 
                 NumTextures = br.ReadUInt32();
                 LumpOffset = br.ReadUInt32();
@@ -129,6 +130,7 @@ namespace Sledge.Packages.Wad
                     paletteDataOffset = br.BaseStream.Position;
                     break;
                 case WadEntryType.Texture:
+                case WadEntryType.QuakeTexture:
                     br.BaseStream.Position += 16; // Skip name
                     width = br.ReadUInt32();
                     height = br.ReadUInt32();
@@ -136,8 +138,16 @@ namespace Sledge.Packages.Wad
                     var num = (int)(width * height);
                     var skipMapData = (num / 4) + (num / 16) + (num / 64);
                     br.BaseStream.Position += 16 + num + skipMapData; // Skip mipmap offsets, texture data, mipmap texture data
-                    paletteSize = br.ReadUInt16();
-                    paletteDataOffset = br.BaseStream.Position;
+                    if (e.Type == WadEntryType.QuakeTexture)
+                    {
+                        paletteSize = 256;
+                        paletteDataOffset = 0;
+                    }
+                    else
+                    {
+                        paletteSize = br.ReadUInt16();
+                        paletteDataOffset = br.BaseStream.Position;
+                    }
                     break;
                     /*
                 case WadEntryType.Font:
