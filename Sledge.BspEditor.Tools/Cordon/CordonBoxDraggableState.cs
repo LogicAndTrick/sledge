a289fde99ac94a2467368b77a1c8c33c3f91ff17
Daniel Walder <daniel.walder@gmail.com>
2017-06-18 14:42:04 +1000

    Cordon tool

---
+++ Sledge.BspEditor.Tools/Cordon/CordonBoxDraggableState.cs
@@ -0,0 +1,41 @@
+﻿using Sledge.BspEditor.Primitives.MapData;
+using Sledge.BspEditor.Rendering.Viewport;
+using Sledge.BspEditor.Tools.Draggable;
+using Sledge.DataStructures.Geometric;
+
+namespace Sledge.BspEditor.Tools.Cordon
+{
+    public class CordonBoxDraggableState : BoxDraggableState
+    {
+        public CordonBoxDraggableState(BaseDraggableTool tool) : base(tool)
+        {
+            // todo render the outer edges?
+            // the old cordon tool used: Color.FromArgb(128, Color.Orange)
+        }
+
+        public void Update()
+        {
+            if (Tool.Document == null)
+            {
+                State.Action = BoxAction.Idle;
+            }
+            else
+            {
+                var cordon = Tool.Document.Map.Data.GetOne<CordonBounds>() ?? new CordonBounds {Enabled = false};
+                State.Start = cordon.Box.Start;
+                State.End = cordon.Box.End;
+                State.Action = BoxAction.Drawn;
+            }
+        }
+
+        public override void Click(MapViewport viewport, ViewportEvent e, Coordinate position)
+        {
+            //
+        }
+
+        public override bool CanDrag(MapViewport viewport, ViewportEvent e, Coordinate position)
+        {
+            return false;
+        }
+    }
+}
---
+++ Sledge.BspEditor.Tools/Cordon/CordonTool.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Drawing;
+using Sledge.BspEditor.Modification;
+using Sledge.BspEditor.Modification.Operations;
+using Sledge.BspEditor.Primitives.MapData;
+using Sledge.BspEditor.Tools.Draggable;
+using Sledge.BspEditor.Tools.Properties;
+using Sledge.Common.Shell.Components;
+using Sledge.DataStructures.Geometric;
+
+namespace Sledge.BspEditor.Tools.Cordon
+{
+    [Export(typeof(ITool))]
+    public class CordonTool : BaseDraggableTool
+    {
+        private readonly CordonBoxDraggableState _cordonBox;
+
+        public CordonTool()
+        {
+            _cordonBox = new CordonBoxDraggableState(this);
+            _cordonBox.BoxColour = Color.Red;
+            _cordonBox.FillColour = Color.FromArgb(/*View.SelectionBoxBackgroundOpacity*/ 64, Color.LightGray);
+            _cordonBox.State.Changed += CordonBoxChanged;
+            States.Add(_cordonBox);
+
+            Usage = ToolUsage.View2D;
+        }
+
+        public override Image GetIcon()
+        {
+            return Resources.Tool_Cordon;
+        }
+
+        public override string GetName()
+        {
+            return "CordonTool";
+        }
+
+        public override void ToolSelected()
+        {
+            _cordonBox.Update();
+            base.ToolSelected();
+        }
+
+        private void CordonBoxChanged(object sender, EventArgs e)
+        {
+            if (_cordonBox.State.Action == BoxAction.Drawn || _cordonBox.State.Action == BoxAction.Resizing)
+            {
+                var bounds = new Box(_cordonBox.State.Start, _cordonBox.State.End);
+                var cb = new CordonBounds
+                {
+                    Box = bounds,
+                    Enabled = Document.Map.Data.GetOne<CordonBounds>()?.Enabled == true
+                };
+                MapDocumentOperation.Perform(Document, new TrivialOperation(x => x.Map.Data.Replace(cb), x => x.Update(cb)));
+            }
+        }
+    }
+}
