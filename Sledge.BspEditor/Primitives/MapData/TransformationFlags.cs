f68ec25b1701a6615914d0959bc6956f93c13278
Daniel Walder <daniel.walder@gmail.com>
2017-08-27 19:03:30 +1000

    Texture lock trial and error testing

---
+++ Sledge.BspEditor/Primitives/MapData/TransformationFlags.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Runtime.Serialization;
+using Sledge.BspEditor.Primitives.MapObjects;
+using Sledge.Common.Transport;
+
+namespace Sledge.BspEditor.Primitives.MapData
+{
+    [Serializable]
+    public class TransformationFlags : IMapData
+    {
+        public bool TextureLock { get; set; } = true;
+        public bool TextureScaleLock { get; set; }
+
+        [Export(typeof(IMapElementFormatter))]
+        public class HideFaceMaskFormatter : StandardMapElementFormatter<TransformationFlags> { }
+        
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("TextureLock", TextureLock);
+            info.AddValue("TextureScaleLock", TextureScaleLock);
+        }
+
+        public SerialisedObject ToSerialisedObject()
+        {
+            var so = new SerialisedObject("TransformationFlags");
+            so.Set("TextureLock", TextureLock);
+            so.Set("TextureScaleLock", TextureScaleLock);
+            return so;
+        }
+
+        public IMapElement Copy(UniqueNumberGenerator numberGenerator)
+        {
+            return Clone();
+        }
+
+        public IMapElement Clone()
+        {
+            return new TransformationFlags
+            {
+                TextureLock = TextureLock,
+                TextureScaleLock = TextureScaleLock
+            };
+        }
+    }
+}
--- Sledge.BspEditor/Primitives/Texture.cs
+++ Sledge.BspEditor/Primitives/Texture.cs
@@ -96,5 +96,60 @@ namespace Sledge.BspEditor.Primitives
                 YScale = YScale
             };
         }
+        
+        /// <summary>
+        /// Transform this texture in a non-destructive way.
+        /// All vertices in the texture must remain unchanged relative to each
+        /// other for this transformation to be valid.
+        /// </summary>
+        /// <param name="matrix">The matrix to transform the texture by</param>
+        public void TransformUniform(Matrix matrix)
+        {
+            #if DEBUG
+
+            // Validate that the transformation is indeed uniform.
+            // If it's not, bail out.
+
+            var one = Coordinate.One * matrix;
+            var two = Coordinate.One * 2 * matrix;
+            if (Math.Abs((two - one).VectorMagnitude() - Coordinate.One.VectorMagnitude()) > 0.0001m)
+                throw new InvalidOperationException("Transform isn't uniform!");
+
+            #endif
+
+            // If the determinant isn't 1, then we can't transform safely.
+            if (Math.Abs(matrix.Determinant() - 1) > 0.0001m) return;
+
+            var startU = UAxis * matrix;
+            var startV = VAxis * matrix;
+
+            var origin = Coordinate.Zero * matrix;
+            UAxis = (startU - origin).Normalise();
+            VAxis = (startV - origin).Normalise();
+
+            if (XScale != 0 && YScale != 0)
+            {
+                XShift -= (startU - UAxis).Dot(UAxis) / XScale;
+                YShift -= (startV - VAxis).Dot(VAxis) / YScale;
+            }
+        }
+
+        /// <summary>
+        /// Rescale this texture based on the given transformation matrix.
+        /// Only scale changes of the transformation are taken into account.
+        /// </summary>
+        /// <param name="matrix">The transformation matrix</param>
+        public void TransformScale(Matrix matrix)
+        {
+            var startU = UAxis * matrix;
+            var startV = VAxis * matrix;
+
+            var origin = Coordinate.Zero * matrix;
+            var deltaU = (startU - origin).VectorMagnitude();
+            var deltaV = (startV - origin).VectorMagnitude();
+
+            if (deltaU != 0) XScale *= deltaU;
+            if (deltaV != 0) YScale *= deltaV;
+        }
     }
 }
