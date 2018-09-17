0a6a7b3daa8f814c1694ed2bcad2d36cd904ff93
Daniel Walder <daniel.walder@gmail.com>
2018-09-17 18:32:35 +1000

    Force decimals in all number formats

--- Sledge.Editor/Program.cs
+++ Sledge.Editor/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Globalization;
+using System.Threading;
 using Sledge.Shell;
 
 namespace Sledge.Editor
@@ -12,6 +10,20 @@ namespace Sledge.Editor
         [STAThread]
         static void Main()
         {
+            // From: https://stackoverflow.com/a/9160150
+            // This is the easiest and most consistent way to force decimals to be used in winforms controls.
+            // Users expect decimals to be displayed, since that's how VHE and other editors behave.
+            // This is a deviation from expected behaviour of an application, which is why this is in
+            // the editor bootstrapper, and not in the shell.
+
+            var forceDecimalCulture = (CultureInfo)Thread.CurrentThread.CurrentCulture.Clone();
+
+            forceDecimalCulture.NumberFormat.NumberDecimalSeparator = ".";
+            forceDecimalCulture.NumberFormat.NumberGroupSeparator = ",";
+
+            Thread.CurrentThread.CurrentCulture = forceDecimalCulture;
+
+            // We're finished ruining the culture, let's run the app now.
             Startup.Run();
         }
     }
