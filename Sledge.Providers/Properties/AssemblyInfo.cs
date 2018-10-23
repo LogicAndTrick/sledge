8762a6de07a9fa486d51aff0913cdc0306fd775c
Daniel Walder <daniel.walder@gmail.com>
2018-10-23 21:13:08 +1000

    2.0.7.2

--- Sledge.Providers/Properties/AssemblyInfo.cs
+++ Sledge.Providers/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.0.7.1")]
-[assembly: AssemblyVersion("2.0.7.1")]
-[assembly: AssemblyFileVersion("2.0.7.1")]
+// [assembly: AssemblyVersion("2.0.7.2")]
+[assembly: AssemblyVersion("2.0.7.2")]
+[assembly: AssemblyFileVersion("2.0.7.2")]
