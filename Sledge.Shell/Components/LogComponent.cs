278d8db087a3116b142d1bf3d0ee4ef203443cce
Daniel Walder <daniel.walder@gmail.com>
2018-10-24 20:00:55 +1000

    Fix threading exceptions in the log buffer

--- Sledge.Shell/Components/LogComponent.cs
+++ Sledge.Shell/Components/LogComponent.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using LogicAndTrick.Oy;
@@ -15,18 +17,18 @@ namespace Sledge.Shell.Components
 {
     [AutoTranslate]
     [Export(typeof(IBottomTabComponent))]
+    [OrderHint("B")]
     public class LogComponent : IBottomTabComponent
     {
-        private TextBox _control;
-        private List<LogMessage> _logs;
+        private readonly TextBox _control;
+        private readonly LogBuffer _logs;
 
         public string Title { get; set; } = "Log";
-        public string OrderHint => "B";
         public object Control => _control;
 
         public LogComponent()
         {
-            _logs = new List<LogMessage>();
+            _logs = new LogBuffer(100);
             _control = new TextBox
             {
                 Dock = DockStyle.Fill,
@@ -39,28 +41,66 @@ namespace Sledge.Shell.Components
             Oy.Subscribe<LogMessage>("Log", AppendLog);
         }
 
-        private async Task AppendLog(LogMessage log)
+        private Task AppendLog(LogMessage log)
         {
-            _logs.Insert(0, log);
-            if (_logs.Count > 100) _logs = _logs.Take(100).ToList();
+            _logs.Add(log);
             var sb = new StringBuilder();
             foreach (var lm in _logs.ToList())
             {
                 sb.AppendFormat("{0} [{1}]: {2}\r\n", lm.Type, lm.Source, lm.Message);
             }
-            if (!_control.Created) return;
 
-            _control.InvokeLater(() =>
+            if (_control.Created)
             {
-                _control.Text = sb.ToString();
-                _control.SelectionStart = 0;
-                _control.ScrollToCaret();
-            });
+                _control.InvokeLater(() =>
+                {
+                    _control.Text = sb.ToString();
+                    _control.SelectionStart = 0;
+                    _control.ScrollToCaret();
+                });
+            }
+
+            return Task.CompletedTask;
         }
 
         public bool IsInContext(IContext context)
         {
             return true;
+        }
+
+        // Based off https://codereview.stackexchange.com/a/134147
+        private class LogBuffer
+        {
+            private readonly ConcurrentQueue<LogMessage> _data;
+            private readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+            private readonly int _size;
+
+            public LogBuffer(int size)
+            {
+                _data = new ConcurrentQueue<LogMessage>();
+                _size = size;
+            }
+
+            public List<LogMessage> ToList()
+            {
+                var list = _data.ToList();
+                list.Reverse();
+                return list;
+            }
+
+            public void Add(LogMessage t)
+            {
+                _lock.EnterWriteLock();
+                try
+                {
+                    if (_data.Count >= _size) _data.TryDequeue(out _);
+                    _data.Enqueue(t);
+                }
+                finally
+                {
+                    _lock.ExitWriteLock();
+                }
+            }
         }
     }
 }
